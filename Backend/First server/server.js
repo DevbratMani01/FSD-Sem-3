de32a0e486944ccc5ede9fe53c3abbb5cb15de7b
DevbratMani01 <parthmani002@gmail.com>
2026-09-08 11:39:44 +0530

    new

---
+++ Backend/First server/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req , res) => {
+    if(req.method == 'GET'){
+        res.writeHead(200 , {'Content-Type' : 'text/plain'});
+        res.end("Hello World")
+    
+    }
+});
+
+server.listen(8000 , () => {
+    console.log("Server is running on port 8000");
+});
